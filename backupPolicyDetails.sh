46b3cd9875a25c1f8804a23b7e12ca0b69dc2ec2
RaviBattula747 <105776414+RaviBattula747@users.noreply.github.com>
2022-07-26 01:04:25 +0530

    Update backupPolicyDetails.sh

--- backupPolicyDetails.sh
+++ backupPolicyDetails.sh
@@ -1,11 +1,14 @@
+
+
 az backup policy list --resource-group rg-bck-prd-01 --vault-name rsv-wus-prd-01 --query [].name -o tsv >> policyList.txt
+
 input1="/home/ravi_battula/policyList.txt"
+
 while read -r liner
-        do
+
+do
         ddId=$liner
         az backup policy list-associated-items --subscription NANTMEDIA-ss—01 --resource-group rg-bck-ss-01 --vault-name rsv-wus-ss-01 --name $ddId --query [].[properties.policyName,properties.friendlyName] -o tsv >> output.txt
-
-
 done < "$input1"
 
 --description
