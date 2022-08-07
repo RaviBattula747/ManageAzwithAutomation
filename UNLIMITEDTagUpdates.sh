80ae211dbf42544a824521530f7afc50e952c5e5
RaviBattula747 <105776414+RaviBattula747@users.noreply.github.com>
2022-08-07 10:51:32 +0530

    Updated

--- UNLIMITEDTagUpdates.sh
+++ UNLIMITEDTagUpdates.sh
@@ -4,11 +4,11 @@ input="/home/ravi_battula/file143.txt"
 
 #Lak[subscription_id]=subscription Name.
 
-Lak[f63fa6ca-967f-4ff6-8e38-554feccee894]="NANTMEDIA-NP—01"
-Lak[74fc6867-eae0-4eb9-9a87-b89164d250dd]="NANTMEDIA-PRD—01"
-Lak[307229a7-f578-4e4c-b96e-522183580ae3]="NANTMEDIA-SANDBOX-01"
-Lak[b3f964c7-1d16-4c8d-8240-dd1b6a30a429]="NANTMEDIA-SS—01"
-Lak[1bffd3a8-c19d-4216-be31-6461c03d2180]="NANTMEDIA-VDI-01"
+Lak[f63fa6ca-967f-4ff6-8e38-************]="NA#T₹E&IA-NP—01"
+Lak[74fc6867-eae0-4eb9-9a87-************]="NA#T₹E&IA-PRD—01"
+Lak[307229a7-f578-4e4c-b96e-************]="NA#T₹E&IA-SANDBOX-01"
+Lak[b3f964c7-1d16-4c8d-8240-************]="NA#T₹E&IA-SS—01"
+Lak[1bffd3a8-c19d-4216-be31-************]="NA#T₹E&IA-VDI-01"
 
   key="Application Name"
   while read -r lines
